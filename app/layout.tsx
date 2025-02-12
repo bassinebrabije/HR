1e441595c93eda854cf3f02f1c4b55de75708353
bassinebrabije <brabije.yassine123@gmail.com>
2025-02-12 02:16:40 +0100

    metatags

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { Analytics } from "@vercel/analytics/react"
 import "./globals.css";
 import Header from "./header";
 
@@ -46,6 +47,7 @@ export default function RootLayout({
       <body className="antialiased">
         <Header />
         {children}
+        <Analytics />
       </body>
     </html>
   );
